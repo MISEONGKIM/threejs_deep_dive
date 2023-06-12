5b4d96069273f74a3a20c7194ecb94326e982b74
MISEONGKIM <rlaaltjd46@naver.com>
2023-06-12 16:06:43 +0900

    feat : 별 class 생성까지만

--- 1-StarlightEarth/src/js/star.js
+++ 1-StarlightEarth/src/js/star.js
@@ -1,12 +1,14 @@
-# threejs 심화 강의
+import * as THREE from "three";
 
-### BufferGeometry()의 position 설정 시 유의점
+export class Star {
+  constructor({ scene }) {
+    this.scene = scene;
 
-- Float32Array() : threejs는 일반배열을 넘겨주면 제대로 된 데이터 파악을 못함. 보다 정확한 데이터 자료형을 넘겨줘야함
+    this.count = 500;
+  }
 
-```
- createStar({ materialOpt, geometryOpt }) {
-
+  createStar({ materialOpt, geometryOpt }) {
+    //threejs는 일반배열을 넘겨주면 제대로된 데이터 파악을 못함. 보다 정확한 데이터 자료형을 넘겨줘야함
     const positions = new Float32Array(this.count * 3);
     for (let i = 0; i < this.count; i++) {
       positions[i] = Math.random() * 3;
@@ -19,4 +21,4 @@
       new THREE.BufferAttribute(positions, 3)
     );
   }
-```
+}
