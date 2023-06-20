387cbca8491a941568333ac0475e5c63a6cdaa2c
MISEONGKIM <rlaaltjd46@naver.com>
2023-06-20 17:52:27 +0900

    feat:html threejs 연동

---
+++ 5-fancy-gallery/README.md
@@ -0,0 +1,50 @@
+# 마크업과 threejs
+
+## 카메라의 위치와 시야각을 html의 사이즈에 맞추기
+
+### 맞춰야하는 이유
+
+- 아래 코드처럼 width, height를 주면 화면에 mesh가 꽉 찬다.
+
+* html에서 표현되는 사이즈와 canvas 단위가 다르다.
+  - 만약에 html에서 width, height값은 1값을 주면 1px로 적용이 됨.
+  - canvas는 카메라 개념이 들어가서 width와 height를 동일하게 주더라도, 카메라의 위치가 canvas의 mesh에 가깝다면 mesh가 더 크게 보이고 ,카메라의 위치가 canvas의 mesh와 거리가 멀어진다면, 사이즈가 동일할 지라도 mesh는 작게 보임
+
+```
+const { width, height, top, left } = image.getBoundingClientRect();
+const material = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+  size: THREE.DoubleSide
+});
+const geometry = new THREE.PlaneGeometry(width, height, 16, 16);
+const mesh = new THREE.Mesh(geometry, material);
+```
+
+### 시야각 계산
+
+설명 : https://www.notion.so/html-71ea1d44b6e643219c98e62c611842a5
+
+```
+  camera.fov = Math.atan(canvasSize.height / 2 / 50) * (180 / Math.PI) * 2;
+```
+
+## threejs 좌표체계 html의 좌표체계로 맞추기
+
+- threejs 좌표체계와 html의 좌표체계가 다름. 만약 (0, 0)이 있다면 threejs에선 정중앙에 위치하고, html에선 좌측상단에 위치함
+- html의 좌표계의 위치에 맞게 좌측상단에 mesh가 위치하도록 아래처럼 조정한다.
+- **Threejs에서 y좌표 +(플러스)하면 위로, (-)하면 아래로, x좌표 +하면 오른쪽, -이면 왼쪽으로 감**
+
+```
+  //html의 (0, 0) 위치로
+    mesh.position.y = canvasSize.height / 2 - height / 2;
+    mesh.position.x = -canvasSize.width / 2 + width / 2;
+```
+
+- top -(마이너스)를 하여 좌측상단에서 top의 크기만큼 아래로 이동
+- left -(마이너스)하여 좌측상단에서 left의 크기만큼 왼쪽으로 이동
+
+```
+  mesh.position.y = canvasSize.height / 2 - height / 2 - top;
+  mesh.position.x = -canvasSize.width / 2 + width / 2 - left;
+```
--- 5-fancy-gallery/src/js/app.js
+++ 5-fancy-gallery/src/js/app.js
@@ -1,5 +1,12 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import vertexShader from "../shaders/vertex.glsl?raw";
+import fragmentShader from "../shaders/fragment.glsl?raw";
+import ASScroll from "@ashthornton/asscroll";
+
+const asscroll = new ASScroll({
+  disableRaf: true
+});
+asscroll.enable();
 
 export default function () {
   const renderer = new THREE.WebGLRenderer({
@@ -22,18 +29,47 @@ export default function () {
     0.1,
     100
   );
-  camera.position.set(0, 0, 3);
+  camera.position.set(0, 0, 50);
+  camera.fov = Math.atan(canvasSize.height / 2 / 50) * (180 / Math.PI) * 2; //시야각
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.1;
+  const imageRepository = [];
+  const loadImages = async () => {
+    const images = [...document.querySelectorAll("main .content img")];
 
-  const createObject = () => {
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const geometry = new THREE.PlaneGeometry(1, 1);
-    const mesh = new THREE.Mesh(geometry, material);
+    const fetchImages = images.map(
+      (image) =>
+        new Promise((resolve, reject) => {
+          image.onload = resolve(image);
+          image.onerror = reject;
+        })
+    );
+    const loadedImages = await Promise.all(fetchImages);
+    return loadedImages;
+  };
 
-    scene.add(mesh);
+  const createImages = (images) => {
+    const imageMeshes = images.map((image) => {
+      const { width, height } = image.getBoundingClientRect();
+      const material = new THREE.ShaderMaterial({
+        vertexShader,
+        fragmentShader,
+        side: THREE.DoubleSide
+      });
+      const geometry = new THREE.PlaneGeometry(width, height, 16, 16);
+      const mesh = new THREE.Mesh(geometry, material);
+
+      imageRepository.push({ img: image, mesh });
+
+      return mesh;
+    });
+
+    return imageMeshes;
+  };
+
+  const create = async () => {
+    const loadedImages = await loadImages();
+    const images = createImages([...loadedImages]);
+    scene.add(...images);
   };
 
   const resize = () => {
@@ -41,6 +77,7 @@ export default function () {
     canvasSize.height = window.innerHeight;
 
     camera.aspect = canvasSize.width / canvasSize.height;
+    camera.fov = Math.atan(canvasSize.height / 2 / 50) * (180 / Math.PI) * 2; //시야각
     camera.updateProjectionMatrix();
 
     renderer.setSize(canvasSize.width, canvasSize.height);
@@ -48,20 +85,34 @@ export default function () {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   };
 
+  const retransform = () => {
+    imageRepository.forEach(({ img, mesh }) => {
+      const { width, height, top, left } = img.getBoundingClientRect();
+      const { width: originWidth } = mesh.geometry.parameters;
+      const scale = width / originWidth;
+      mesh.scale.x = scale;
+      mesh.scale.y = scale;
+
+      mesh.position.y = canvasSize.height / 2 - height / 2 - top;
+      mesh.position.x = -canvasSize.width / 2 + width / 2 - left;
+    });
+  };
+
   const addEvent = () => {
     window.addEventListener("resize", resize);
   };
 
   const draw = () => {
-    controls.update();
     renderer.render(scene, camera);
+    retransform();
+    asscroll.update();
     requestAnimationFrame(() => {
       draw();
     });
   };
 
   const initialize = () => {
-    createObject();
+    create();
     addEvent();
     resize();
     draw();
