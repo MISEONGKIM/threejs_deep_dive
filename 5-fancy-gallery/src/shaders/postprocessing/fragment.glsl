4897e183302060c3bcf64f9ff7b2a668fe2c0eb3
MISEONGKIM <rlaaltjd46@naver.com>
2023-06-21 18:00:43 +0900

    chore : 노이즈 설명 추가 중임

--- 5-fancy-gallery/src/shaders/postprocessing/fragment.glsl
+++ 5-fancy-gallery/src/shaders/postprocessing/fragment.glsl
@@ -21,15 +21,23 @@ varying vec2 vUv;
 // }
 
 // 2.side 
-void main() {
-  vec2 newUv = vUv;
-  // float side = smoothstep(0.2, 0.0, newUv.x) + smoothstep(0.8, 1.0, newUv.x);
-  // newUv.y -= (newUv.y - 0.5) * side * 0.1;
-  // 0 ~ 0.4 사이에는 색상이 보이고 0.4 ~ 1 사이에는 검은색
-  float side = smoothstep(0.4, 0.0, newUv.y);
-  newUv.x -= (newUv.x - 0.5) * side * 0.1;
+// void main() {
+//   vec2 newUv = vUv;
+//   // float side = smoothstep(0.2, 0.0, newUv.x) + smoothstep(0.8, 1.0, newUv.x);
+//   // newUv.y -= (newUv.y - 0.5) * side * 0.1;
+//   // 0 ~ 0.4 사이에는 색상이 보이고 0.4 ~ 1 사이에는 검은색
+//   float side = smoothstep(0.4, 0.0, newUv.y);
+//   newUv.x -= (newUv.x - 0.5) * side * 0.1;
   
-  vec4 tex = texture2D(tDiffuse, newUv);
+//   vec4 tex = texture2D(tDiffuse, newUv);
    
-  gl_FragColor = tex ;
+//   gl_FragColor = tex ;
+// }
+
+// 3. noise 
+void main () {
+  vec2 newUv = vUv;
+  vec4 tex = texture2D(tDiffuse, newUv);
+
+  gl_FragColor = tex;
 }
