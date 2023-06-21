2fc023ea24e7bc6b03dc65510b64ee216bacdc34
MISEONGKIM <rlaaltjd46@naver.com>
2023-06-21 10:36:41 +0900

    feat : 이미지 hover시 물결효과 추가

--- 5-fancy-gallery/src/js/app.js
+++ 5-fancy-gallery/src/js/app.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import vertexShader from "../shaders/vertex.glsl?raw";
 import fragmentShader from "../shaders/fragment.glsl?raw";
 import ASScroll from "@ashthornton/asscroll";
+import gsap from "gsap";
 
 const asscroll = new ASScroll({
   disableRaf: true
@@ -22,6 +23,8 @@ export default function () {
     height: window.innerHeight
   };
 
+  const clock = new THREE.Clock();
+  const textureLoader = new THREE.TextureLoader();
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -48,15 +51,30 @@ export default function () {
   };
 
   const createImages = (images) => {
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        uTexture: {
+          value: null
+        },
+        uTime: {
+          value: 0
+        },
+        uHover: {
+          value: 0
+        }
+      },
+      vertexShader,
+      fragmentShader,
+      side: THREE.DoubleSide
+    });
+
     const imageMeshes = images.map((image) => {
       const { width, height } = image.getBoundingClientRect();
-      const material = new THREE.ShaderMaterial({
-        vertexShader,
-        fragmentShader,
-        side: THREE.DoubleSide
-      });
+      const clonedMaterial = material.clone();
+      clonedMaterial.uniforms.uTexture.value = textureLoader.load(image.src);
+
       const geometry = new THREE.PlaneGeometry(width, height, 16, 16);
-      const mesh = new THREE.Mesh(geometry, material);
+      const mesh = new THREE.Mesh(geometry, clonedMaterial);
 
       imageRepository.push({ img: image, mesh });
 
@@ -100,19 +118,41 @@ export default function () {
 
   const addEvent = () => {
     window.addEventListener("resize", resize);
+    imageRepository.forEach(({ img, mesh }) => {
+      // 이렇게 적용해도 바로 적용안됨. canvas(#container)가 img 보다 위에 있어서, css에서 #container의 z-index를 -1로 설정해줘야함.
+      img.addEventListener("mouseenter", () => {
+        //그냥 1로 바로 주면 갑자기 이벤트가 발생해 부자연스러워서 서서히 발생하도록 gsap으로 효과 추가
+        gsap.to(mesh.material.uniforms.uHover, {
+          value: 1,
+          duration: 0.4,
+          ease: "power1.inOut"
+        });
+      });
+      img.addEventListener("mouseout", () => {
+        gsap.to(mesh.material.uniforms.uHover, {
+          value: 0,
+          duration: 0.4,
+          ease: "power1.inOut"
+        });
+      });
+    });
   };
 
   const draw = () => {
     renderer.render(scene, camera);
     retransform();
     asscroll.update();
+    imageRepository.forEach(({ mesh }) => {
+      mesh.material.uniforms.uTime.value = clock.getElapsedTime();
+    });
+
     requestAnimationFrame(() => {
       draw();
     });
   };
 
-  const initialize = () => {
-    create();
+  const initialize = async () => {
+    await create();
     addEvent();
     resize();
     draw();
--- 5-fancy-gallery/src/shaders/fragment.glsl
+++ 5-fancy-gallery/src/shaders/fragment.glsl
@@ -1,5 +1,26 @@
+uniform sampler2D uTexture;
+uniform float uTime;
+uniform float uHover;
+
 varying vec2 vUv;
 
 void main() {
-  gl_FragColor = vec4(vUv, 0.0, 1.0);
+  // vUv : 0 ~ 1
+  vec2 toCenter = vUv - 0.5; // -0.5 ~ 0.5
+  float dist = length(vUv - 0.5); // 0 ~ 0.5;
+  //방향에 따른 질감을 추가하기 위해 내적 사용 , 좀더 자연스러운 웨이브를 위해서 사용 
+  float dir = dot(toCenter, vec2(1.0, 1.0));
+  float strength = 0.5; 
+
+  // 0 ~ 0.5 * 20 => 0 ~ 10
+    // 물결이 시간에따라 치는 느낌을 주기 위해 uTime 사용
+  vec2 wave = vec2(sin(dist* 20.0 - uTime * 5.0), cos(dist* 20.0 - uTime * 5.0));
+
+  // dir안쓰면 중심으로부터 웨이브가 발생해서 중심을 기준으로 원처럼 형성됨.
+  // dist 중심으로 부터 거리가 커짐에 따라 웨이브가 커지게 하기 위해서 사용
+  //uHover가 0이면 vUv 그대로, 1이면 효과 적용
+  vec2 newUv = vUv +  wave * strength * dir * dist * uHover;
+
+  vec4 tex = texture2D(uTexture, newUv);
+  gl_FragColor = tex;
 }
